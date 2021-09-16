136d022160b5f4dc757ef28bdf2931dcb9cd3e63
Karim-mo <karim.mohamed420@gmail.com>
2021-09-16 22:52:37 +0200

    passport authentication modification

--- auth/jwt strategy/jwt.js
+++ auth/jwt strategy/jwt.js
@@ -2,6 +2,8 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import User from '../../db/schemas/User.js';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import passport from 'passport';
+import asyncHandler from 'express-async-handler';
 
 dotenv.config();
 
@@ -11,13 +13,36 @@ const options = {
 };
 
 // Configure the global passport sent by the driver code to verify our jwt token using our symmetric key
-const verifyToken = (passport) => {
+const verifyToken = () => {
 	passport.use(
-		new Strategy(options, function (jwt_payload, done) {
-			console.log(jwt_payload);
-		})
+		new Strategy(
+			options,
+			asyncHandler(async (jwt_payload, done) => {
+				const user = await User.findById(jwt_payload.id);
+				if (user) {
+					return done(null, user);
+				} else {
+					throw new Error('Token verification failed.');
+				}
+			})
+		)
 	);
 };
+
+// Initialize the passport middleware
+verifyToken();
+
+// Middleware to protect private routes to logged in users using passport's authenticate
+const protectedRoute = asyncHandler(async (req, res, next) => {
+	passport.authenticate('jwt', { session: false }, (err, user, info) => {
+		if (!user || err) {
+			throw new Error('Authentication failed');
+		} else {
+			req.user = user;
+			next();
+		}
+	})(req, res, next);
+});
 
 // Issue and sign a jwt token using our symmetric key
 const signToken = (user) =>
@@ -25,6 +50,6 @@ const signToken = (user) =>
 		expiresIn: '1d',
 	});
 
-const exports = { verifyToken, signToken };
+const exports = { verifyToken, signToken, protectedRoute };
 
 export default exports;
--- validation/validation.js
+++ validation/validation.js
@@ -1,8 +1,9 @@
 import expressValidator from 'express-validator';
+import asyncHandler from 'express-async-handler';
 
 // Takes in an array of validations and verifies each of them, aborting when a problem is detected
 const validate = (validations) => {
-	return async (req, res, next) => {
+	return asyncHandler(async (req, res, next) => {
 		await Promise.all(validations.map((validation) => validation.run(req)));
 
 		const errors = expressValidator.validationResult(req);
@@ -11,7 +12,7 @@ const validate = (validations) => {
 		}
 
 		throw new Error('Incorrect/Empty Inputs');
-	};
+	});
 };
 
 export default validate;
